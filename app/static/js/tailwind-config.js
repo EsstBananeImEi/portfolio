177714eae2dd99cba0e1842bf6346f43f22acf1f
Sebastian Meine <sebastian.meine@ilume.de>
2025-02-04 10:02:39 +0100

    Refactor: Move Tailwind config to external JS file

---
+++ app/static/js/tailwind-config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2F3C7E',   /* Tiefes Blau */
+                accent: '#F96167',    /* Sanftes Korallenrot */
+                background: '#F8F8F8',
+                neutral: '#333333',
+                gray: '#E5E5E5'
+            },
+            inset: {
+                'custom': '-5px',
+            }
+        },
+    }
+};
